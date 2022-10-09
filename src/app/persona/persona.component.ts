d9ccb8964f5c3ed618c3b02965fc89d8f1f0969e
dougCast009 <31898972+dougCast009@users.noreply.github.com>
2022-10-09 08:02:51 -0600

    Fin de Proyecto

--- src/app/persona/persona.component.ts
+++ src/app/persona/persona.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./persona.component.css']
 })
 export class PersonaComponent implements OnInit {
+  hobbies: any[] = ['Salir en Bicicleta', 'Jugar Play', 'Hacer Ejercicio', 'Ver Netflix'];
+  edad = 24;
 
+  amuentarEdad(){
+    this.edad = this.edad + 1;
+  }
+
+  restarEdad(){
+    this.edad = this.edad - 1;
+  }
   constructor() { }
 
   ngOnInit(): void {
+    this.hobbies;
   }
 
 }
